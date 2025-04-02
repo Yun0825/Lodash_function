d33485a81c884d471592b709b787dae17afc732d
HYEONGYUN\guddb <guddbs5545@naver.com>
2025-04-02 16:31:13 +0900

    feat: each 함수 구현

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Lodash_function
+## Lodash 함수 직접 구현해보는 프로젝트
---
+++ each.js
@@ -0,0 +1,7 @@
+const _ = {};
+_.each = function (array, iterator) {
+    const len = array.length;
+    for (let i = 0; i < len; i++) {
+        iterator(array[i], i, array)
+    }
+};
